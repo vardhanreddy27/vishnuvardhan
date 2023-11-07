13f0ddd093558b64741587d525b04d31774329e5
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2023-11-07 09:33:48 +0530

    my portfolios

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,8 +4,8 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
-				<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js" async></script>
-				<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js" async></script>
+				<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js" strategy="beforeInteractive" async></script>
+				<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js" strategy="beforeInteractive" async></script>
 				<Script
 					src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
 					integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
